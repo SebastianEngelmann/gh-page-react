b71634521af28e2e72ce7d8506605d078285fad7
Sebastian Engelmann <s.engelmann@fu-berlin.de>
2021-03-01 17:48:27 +0100

    updated homepage

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My awesome GitHUb Page running using React
         </p>
         <a
           className="App-link"
